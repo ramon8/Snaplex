e53f09b1166f37b93fa25d78f4158e942ed50c9a
unknown <ramon.fer96@gmail.com>
2022-12-02 17:00:46 +0100

    chore: removed get

--- apps/snaplex-server/src/main.ts
+++ apps/snaplex-server/src/main.ts
@@ -2,18 +2,12 @@ import express from 'express';
 import http from 'http';
 import { Server } from "socket.io";
 
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:4200"
     }
-});
-
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
 });
 
 io.on('connection', (socket) => {
